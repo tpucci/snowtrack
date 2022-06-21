edc93756087928caab51c7f197d55c02c0936fc6
Thomas Pucci <thomas.lb.pucci@gmail.com>
2022-06-21 09:58:43 +0200

    feat: change accessibility on text field

--- lib/snowtrack_web/live/components/text_field.ex
+++ lib/snowtrack_web/live/components/text_field.ex
@@ -1,7 +1,7 @@
 defmodule SnowtrackWeb.Components.TextField do
   use SnowtrackWeb, :component
 
-  @default_class "appearance-none relative block w-full px-3 py-2 border border-background-50/[0.1] placeholder-background-500 text-white rounded-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:z-10 sm:text-sm bg-background-800"
+  @default_class "appearance-none relative block w-full px-3 py-2 border border-background-50/[0.1] placeholder-background-500 text-white rounded-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:z-10 text-sm bg-background-800"
 
   def textfield(assigns) do
     assigns = assigns |> assign_new(:class, fn -> @default_class end)
