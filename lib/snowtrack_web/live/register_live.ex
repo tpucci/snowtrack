c6164aeedcf739ef191fad3c7c1d2df0120a2f56
Thomas Pucci <thomas.lb.pucci@gmail.com>
2022-06-22 19:14:18 +0200

    feat: add success message

--- lib/snowtrack_web/live/register_live.ex
+++ lib/snowtrack_web/live/register_live.ex
@@ -7,42 +7,63 @@ defmodule SnowtrackWeb.RegisterLive do
   def render(assigns) do
     debounce = if assigns.changeset.action, do: nil, else: "blur"
 
+    title =
+      if assigns.changeset.action == :success,
+        do: dgettext("accounts", "Confirm your email"),
+        else: dgettext("accounts", "Register and start monitoring your apps")
+
     ~H"""
     <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div class="max-w-md w-full space-y-8">
         <div>
           <img class="mx-auto h-12 w-auto" src="/images/icon/snowtrack.svg" alt="Snowtrack logo" />
           <h1 class="mt-6 text-center text-3xl font-bold text-white">
-            <%= dgettext("accounts", "Register and start monitoring your apps") %>
+            <%= title %>
           </h1>
         </div>
-        <.form let={f} for={@changeset} phx-change="validate" phx-submit="save" class="mt-8 space-y-6">
-          <div class="space-y-3">
-            <.textfield
-              form={f}
-              field={:email}
-              field_type={:email}
-              label={dgettext("accounts", "Email address")}
-              placeholder={dgettext("accounts", "Email address")}
-              debounce={debounce}
-            />
+        <%= if @changeset.action == :success do %>
+          <p>
+            <%= dgettext(
+              "accounts",
+              "We successfully created your account ! Check your mailbox (%{email}) and visit the link we sent you to confirm your email address.",
+              email: @changeset["user"]["email"]
+            ) %>
+          </p>
+        <% else %>
+          <.form
+            let={f}
+            for={@changeset}
+            phx-change="validate"
+            phx-submit="save"
+            class="mt-8 space-y-6"
+          >
+            <div class="space-y-3">
+              <.textfield
+                form={f}
+                field={:email}
+                field_type={:email}
+                label={dgettext("accounts", "Email address")}
+                placeholder={dgettext("accounts", "Email address")}
+                debounce={debounce}
+              />
 
-            <.textfield
-              form={f}
-              field={:password}
-              field_type={:password}
-              label={dgettext("accounts", "Password")}
-              placeholder={dgettext("accounts", "Password")}
-              debounce={debounce}
-            />
-          </div>
+              <.textfield
+                form={f}
+                field={:password}
+                field_type={:password}
+                label={dgettext("accounts", "Password")}
+                placeholder={dgettext("accounts", "Password")}
+                debounce={debounce}
+              />
+            </div>
 
-          <.submitbtn
-            label={dgettext("accounts", "Register")}
-            icon={&ic_lock_open/1}
-            label_disable_with={dgettext("accounts", "Registering...")}
-          />
-        </.form>
+            <.submitbtn
+              label={dgettext("accounts", "Register")}
+              icon={&ic_lock_open/1}
+              label_disable_with={dgettext("accounts", "Registering...")}
+            />
+          </.form>
+        <% end %>
       </div>
     </div>
     """
@@ -63,8 +84,8 @@ defmodule SnowtrackWeb.RegisterLive do
     {:noreply, assign(socket, changeset: changeset)}
   end
 
-  def handle_event("save", %{"user" => user_params}, socket) do
-    case Accounts.register_user(user_params) do
+  def handle_event("save", changeset, socket) do
+    case Accounts.register_user(changeset["user"]) do
       {:ok, user} ->
         {:ok, _} =
           Accounts.deliver_user_confirmation_instructions(
@@ -74,8 +95,8 @@ defmodule SnowtrackWeb.RegisterLive do
 
         {:noreply,
          socket
-         |> put_flash(:info, "user created")
-         |> redirect(to: Routes.user_session_path(socket, :new))}
+         |> put_flash(:info, dgettext("accounts", "You have been registered"))
+         |> assign(changeset: changeset |> Map.put(:action, :success))}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
