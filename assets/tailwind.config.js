65b6ec921dcded85060a3c64d5dc78943d58444b
Thomas Pucci <thomas.lb.pucci@gmail.com>
2022-06-22 18:35:27 +0200

    feat: customize flash messages

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -16,11 +16,31 @@ module.exports = {
       },
       animation: {
         appear: "scale-y 1s ease-out 1",
+        notify:
+          "slide-from-right .3s cubic-bezier(.24,1.66,.75,.78) forwards, scale .3s cubic-bezier(.24,1.66,.75,.78) forwards, fade-in .5s ease-out forwards",
       },
       keyframes: {
         "scale-y": {
           "0%": { "max-height": "0", overflow: "hidden", opacity: 0 },
           "100%": { "max-height": "300px", opacity: 1 },
+        },
+        flash: {
+          "0%": { right: "0", opacity: 0 },
+          "10%": { right: "1em", opacity: 1 },
+          "90%": { right: "1em", opacity: 1 },
+          "100%": { right: "0", opacity: 0, visibility: "hidden" },
+        },
+        "slide-from-right": {
+          "0%": { right: "-4em" },
+          "100%": { right: "1em" },
+        },
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        scale: {
+          "0%": { transform: "scale(0.5)" },
+          "100%": { transform: "scale(1)" },
         },
       },
     },
