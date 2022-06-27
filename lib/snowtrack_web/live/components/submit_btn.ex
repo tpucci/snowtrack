00f74c6f8b346c23c4f3de2ee887c87e1cd938b6
Thomas Pucci <thomas.lb.pucci@gmail.com>
2022-06-27 11:51:15 +0200

    feat: remove icon optionnally

--- lib/snowtrack_web/live/components/submit_btn.ex
+++ lib/snowtrack_web/live/components/submit_btn.ex
@@ -1,4 +1,6 @@
 defmodule SnowtrackWeb.Components.SubmitBtn do
+  @moduledoc false
+
   use SnowtrackWeb, :component
 
   @default_class "group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary-600 hover:bg-primary-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
@@ -8,9 +10,11 @@ defmodule SnowtrackWeb.Components.SubmitBtn do
 
     ~H"""
     <%= submit class: @class, phx_disable_with: @label_disable_with do %>
-      <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-        <%= component(@icon, class: "h-5 w-5 text-primary-500 group-hover:text-primary-300") %>
-      </span>
+      <%= if Map.has_key?(assigns, :icon) do %>
+        <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+          <%= component(@icon, class: "h-5 w-5 text-primary-500 group-hover:text-primary-300") %>
+        </span>
+      <% end %>
       <%= @label %>
     <% end %>
     """
