b352bc7b44a71a513694123d6691a52f29b401a2
Thomas Pucci <thomas.lb.pucci@gmail.com>
2022-07-10 22:06:13 +0200

    feat: hide flash after 8 sec

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,10 +22,29 @@ import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
 
+let Hooks = {};
+
+Hooks.Flash = {
+  mounted() {
+    let hide = () =>
+      liveSocket.execJS(this.el, this.el.getAttribute("phx-click"));
+    this.timer = setTimeout(() => hide(), 8000);
+    this.el.addEventListener("phx:hide-start", () => clearTimeout(this.timer));
+    this.el.addEventListener("mouseover", () => {
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => hide(), 8000);
+    });
+  },
+  destroyed() {
+    clearTimeout(this.timer);
+  },
+};
+
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute("content");
 let liveSocket = new LiveSocket("/live", Socket, {
+  hooks: Hooks,
   params: { _csrf_token: csrfToken },
 });
 
--- lib/snowtrack_web/components/flash.ex
+++ lib/snowtrack_web/components/flash.ex
@@ -1,6 +1,8 @@
 defmodule SnowtrackWeb.Components.Flash do
   use SnowtrackWeb, :component
 
+  alias Phoenix.LiveView.JS
+
   import SnowtrackWeb.Components.Icon
 
   def flash(assigns) do
@@ -14,12 +16,18 @@ defmodule SnowtrackWeb.Components.Flash do
     <%= if live_flash(@flash, @kind) do %>
       <div class="sticky top-0 max-w-screen-xl mx-auto z-10">
         <p
+          phx-hook="Flash"
+          id={"flash_#{@kind |> Atom.to_string()}"}
+          phx-click={
+            JS.push("lv:clear-flash")
+            |> JS.remove_class("animate-notify")
+            |> hide("#flash_#{@kind |> Atom.to_string()}")
+          }
+          phx-value-key={@kind |> Atom.to_string()}
           class={
             "group absolute top-16 right-0 mx-4 animate-notify drop-shadow-lg pr-4 pl-2 py-2 bg-background-800 rounded-md border border-background-50/[0.1] border-l-4 #{color} text-gray-100 hover:text-gray-50 hover:bg-background-700 flex items-center cursor-pointer text-sm max-w-prose"
           }
           role="alert"
-          phx-click="lv:clear-flash"
-          phx-value-key={@kind |> Atom.to_string()}
         >
           <span class={"absolute -left-1 inset-y-1/4 border-l-4 rounded-md #{color} animate-ping"} />
           <%= component(icon, class: "h-6 w-6 mr-2 inline shrink-0") %>
@@ -29,5 +37,15 @@ defmodule SnowtrackWeb.Components.Flash do
       </div>
     <% end %>
     """
+  end
+
+  def hide(js \\ %JS{}, selector) do
+    JS.hide(js,
+      to: selector,
+      time: 300,
+      transition:
+        {"transition ease-in duration-300", "transform origin-center opacity-100 scale-100",
+         "transform origin-center opacity-0 scale-95"}
+    )
   end
 end
